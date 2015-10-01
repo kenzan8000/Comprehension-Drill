e97ef20a1311c67cc68ffcabee8a8a1c1c99ee60
kenzan8000 <kenzan8000@gmail.com>
2015-10-01 23:46:11 +0900

    add update web_site model

--- server/app/models/web_site.rb
+++ server/app/models/web_site.rb
@@ -3,6 +3,15 @@ class WebSite < ActiveHash::Base
 
   self.data = [
     # id, link, title, description, language, rss
-    { :id => 1, :link => 'http://www.breakingnewsenglish.com', :title => 'www.BreakingNewsEnglish.com', :description => 'Simple news casts for learners of English. Read the news article at http://www.breakingnewsenglish.com/ . You can also try some vocabulary exercises.', :language => 'en-us', :rss => 'http://www.breakingnewsenglish.com/bne.rss' }
+    {
+      :id => 1,
+      :link => 'http://www.breakingnewsenglish.com',
+      :title => 'www.BreakingNewsEnglish.com',
+      :description => 'Simple news casts for learners of English. Read the news article at http://www.breakingnewsenglish.com/ . You can also try some vocabulary exercises.',
+      :language => 'en-us',
+      :rss => 'http://www.breakingnewsenglish.com/bne.rss',
+      :article_body_xpath => '/html/body/center/table[4]/tr[1]/td/div[1]/table[2]/tr/td[1]/blockquote',
+      :article_question_xpath => '/html/body/center/table[4]/tr[2]/td/blockquote[3]/table[2]'
+    }
   ]
 end
--- server/lib/tasks/scraping_articles_task.rb
+++ server/lib/tasks/scraping_articles_task.rb
@@ -7,8 +7,8 @@ class Tasks::ScrapingArticlesTask
     agent = Mechanize.new
     #page = agent.get('http://www.breakingnewsenglish.com/1509/150929-the-lost-city-of-z.html')
     page = agent.get('http://www.breakingnewsenglish.com/1509/150926-selfies.html')
+
     init_page = Yasuri.pages_init '/html', limit:1 do
-      text_title '/html/head/title'
       text_body '/html/body/center/table[4]/tr[1]/td/div[1]/table[2]/tr/td[1]/blockquote'
       text_question '/html/body/center/table[4]/tr[2]/td/blockquote[3]/table[2]'
     end
