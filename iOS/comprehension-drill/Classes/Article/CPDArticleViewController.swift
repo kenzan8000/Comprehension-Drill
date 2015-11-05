7247e5010c1065c3ea33c36c0d997a0e1598c678
kenzan8000 <kenzan8000@gmail.com>
2015-11-04 23:16:13 -0800

    add TTS play

--- iOS/comprehension-drill/Classes/Article/CPDArticleViewController.swift
+++ iOS/comprehension-drill/Classes/Article/CPDArticleViewController.swift
@@ -1,13 +1,17 @@
 import UIKit
+import AVFoundation
 
 
 class CPDArticleViewController: UIViewController {
 
     /// MARK: - property
+
     var article: CPDArticle!
+    var speechSynthesizer = AVSpeechSynthesizer()
 
 
     /// MARK: - life cycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
         //CPDLOG(self.article)
@@ -26,5 +30,31 @@ class CPDArticleViewController: UIViewController {
 
 
     /// MARK: - private api
+
+    /**
+     * play TTS
+     **/
+    private func play() {
+        let utterance = AVSpeechUtterance(string: self.article.body)
+        utterance.voice = AVSpeechSynthesisVoice(language: self.article.language)
+        utterance.rate = 0.2
+        utterance.pitchMultiplier = AVSpeechUtteranceMaximumSpeechRate
+        self.speechSynthesizer.speakUtterance(utterance)
+    }
+
+    /**
+     * stop TTS
+     **/
+    private func stop() {
+        self.speechSynthesizer.pauseSpeakingAtBoundary(AVSpeechBoundary.Immediate)
+    }
+
+/*
+    public func stopSpeakingAtBoundary(boundary: AVSpeechBoundary) -> Bool
+    public func pauseSpeakingAtBoundary(boundary: AVSpeechBoundary) -> Bool
+    public func continueSpeaking() -> Bool
+    public var speaking: Bool { get }
+    public var paused: Bool { get }
+*/
 
 }
